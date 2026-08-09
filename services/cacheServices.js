a1ec40f3a843dc86ba823fa3dbdcce3653c387aa
Harshal135701 <borseharshal1357@gmail.com>
2026-08-09 13:31:47 +0530

    Feat:Fix the redis issue for cache memory of ai response by making redis unavaible for some time

--- services/cacheServices.js
+++ services/cacheServices.js
@@ -1,18 +1,27 @@
 const redisClient = require("../config/redis");
 
 async function getCache(key) {
-    const data = await redisClient.get(key);
-    return data ? JSON.parse(data) : null;
+    try {
+        const data = await redisClient.get(key);
+        return data ? JSON.parse(data) : null;
+    } catch (error) {
+        console.log("Redis unavailable, skipping cache read");
+        return null;
+    }
 }
 
-async function setCache(key, value, expiry = 60*60*24) {
-    await redisClient.set(
-        key,
-        JSON.stringify(value),
-        {
-            EX: expiry
-        }
-    );
+async function setCache(key, value, expiry = 60 * 60 * 24) {
+    try {
+        await redisClient.set(
+            key,
+            JSON.stringify(value),
+            {
+                EX: expiry
+            }
+        );
+    } catch (error) {
+        console.log("Redis unavailable, skipping cache write");
+    }
 }
 
 module.exports = {
